5aa6983b03b4f6a358399f0a49415a2a84f942de
kcubey <keziakew98@gmail.com>
2017-06-30 12:34:04 +0800

    b home

--- SecureNet/SecureNet/Pages/Browser/BrowserHome.xaml.cs
+++ SecureNet/SecureNet/Pages/Browser/BrowserHome.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using VirusTotalNET;
+using VirusTotalNET.Objects;
+using VirusTotalNET.ResponseCodes;
+using VirusTotalNET.Results;
 
 namespace SecureNet.Pages.Browser
 {
@@ -36,10 +40,121 @@ namespace SecureNet.Pages.Browser
             Console.WriteLine("redirecting now");
         }
 
+        private void tempVT(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("WIP");
+        }
+
+
         private void startVT(object sender, RoutedEventArgs e)
         {
             VirusTotal virusTotal = new VirusTotal(ConfigurationManager.AppSettings["virusTotalAPIKey"].ToString());
 
         }
+
+//======================TESTING
+#region VT test
+        private const string ScanUrl = "http://www.google.com/";
+
+        private void VT(object sender, RoutedEventArgs e)
+        {
+            VTasync().Wait();
+
+        }
+
+        private async Task VTasync()
+        {
+            VirusTotal virusTotal = new VirusTotal("6272a916af066e5af228854976345296bbb9a55bde176fc4752c9532899054d6");
+
+            //Use HTTPS instead of HTTP
+            virusTotal.UseTLS = true;
+
+            //Create the EICAR test virus. See http://www.eicar.org/86-0-Intended-use.html
+            FileInfo fileInfo = new FileInfo("EICAR.txt");
+            File.WriteAllText(fileInfo.FullName, @"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*");
+
+            //Check if the file has been scanned before.
+            FileReport fileReport = await virusTotal.GetFileReport(fileInfo);
+
+            bool hasFileBeenScannedBefore = fileReport.ResponseCode == ReportResponseCode.Present;
+
+            Console.WriteLine("File has been scanned before: " + (hasFileBeenScannedBefore ? "Yes" : "No"));
+
+            //If the file has been scanned before, the results are embedded inside the report.
+            if (hasFileBeenScannedBefore)
+            {
+                PrintScan(fileReport);
+            }
+            else
+            {
+                ScanResult fileResult = await virusTotal.ScanFile(fileInfo);
+                PrintScan(fileResult);
+            }
+
+            Console.WriteLine();
+
+            UrlReport urlReport = await virusTotal.GetUrlReport(ScanUrl);
+
+            bool hasUrlBeenScannedBefore = urlReport.ResponseCode == ReportResponseCode.Present;
+            Console.WriteLine("URL has been scanned before: " + (hasUrlBeenScannedBefore ? "Yes" : "No"));
+
+            //If the url has been scanned before, the results are embedded inside the report.
+            if (hasUrlBeenScannedBefore)
+            {
+                PrintScan(urlReport);
+            }
+            else
+            {
+                UrlScanResult urlResult = await virusTotal.ScanUrl(ScanUrl);
+                PrintScan(urlResult);
+            }
+        }
+
+        private static void PrintScan(UrlScanResult scanResult)
+        {
+            Console.WriteLine("Scan ID: " + scanResult.ScanId);
+            Console.WriteLine("Message: " + scanResult.VerboseMsg);
+            Console.WriteLine();
+        }
+
+        private static void PrintScan(ScanResult scanResult)
+        {
+            Console.WriteLine("Scan ID: " + scanResult.ScanId);
+            Console.WriteLine("Message: " + scanResult.VerboseMsg);
+            Console.WriteLine();
+        }
+
+        private static void PrintScan(FileReport fileReport)
+        {
+            Console.WriteLine("Scan ID: " + fileReport.ScanId);
+            Console.WriteLine("Message: " + fileReport.VerboseMsg);
+
+            if (fileReport.ResponseCode == ReportResponseCode.Present)
+            {
+                foreach (KeyValuePair<string, ScanEngine> scan in fileReport.Scans)
+                {
+                    Console.WriteLine("{0,-25} Detected: {1}", scan.Key, scan.Value.Detected);
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void PrintScan(UrlReport urlReport)
+        {
+            Console.WriteLine("Scan ID: " + urlReport.ScanId);
+            Console.WriteLine("Message: " + urlReport.VerboseMsg);
+
+            if (urlReport.ResponseCode == ReportResponseCode.Present)
+            {
+                foreach (KeyValuePair<string, ScanEngine> scan in urlReport.Scans)
+                {
+                    Console.WriteLine("{0,-25} Detected: {1}", scan.Key, scan.Value.Detected);
+                }
+            }
+
+            Console.WriteLine();
+        }
     }
+#endregion
 }
