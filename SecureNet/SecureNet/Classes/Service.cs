9c7113a96297e2adfbf5cf44d24446aa3d19d34d
xdecosee <finalfantasy_1998@hotmail.com>
2017-07-02 00:45:07 +0800

    implemented encryption agorithim

---
+++ SecureNet/SecureNet/Classes/Service.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.IO;
+using System.Data.SqlTypes;
+
+namespace SecureNet.Classes
+{
+    class Service
+    {
+
+        public string name { get; set; }
+        public string url { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string notes { get; set; }
+       
+
+
+
+
+        //Add Service
+        private static int insertService(string nameFinal, string urlFinal, byte[] usernameBytes, byte[] passwdBytes, byte[] notesBytes, int userId)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "Insert Into Service(serviceName, serviceUrl, serviceUsername, servicePassword, serviceNotes, userId)" +
+                " Values(cast(@serviceName as nvarchar(50))," +
+                " cast(@serviceUrl as nvarchar(150))," +
+                " @serviceUsername, @servicePassword, @serviceNotes, @userId);SELECT SCOPE_IDENTITY();";
+            cmd.Parameters.AddWithValue("@serviceName", nameFinal);
+            cmd.Parameters.AddWithValue("@serviceUrl", urlFinal);
+            cmd.Parameters.AddWithValue("@serviceUsername", usernameBytes);
+            cmd.Parameters.AddWithValue("@servicePassword", passwdBytes);
+            cmd.Parameters.AddWithValue("@userId", userId);
+            if (notesBytes != null) {
+                cmd.Parameters.AddWithValue("@serviceNotes", notesBytes);
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@serviceNotes", SqlBinary.Null);
+            }
+            cmd.Connection = GetConnection();
+
+            
+           int serviceId = Convert.ToInt32(cmd.ExecuteScalar());
+           
+            cmd.Connection.Close();
+
+            return serviceId;
+        }
+
+        //Connection
+        private static SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["SecureNetCon"].ConnectionString);
+            connection.Open();
+            return connection;
+        }
+
+        private static void insertKey(int serviceId, byte[] aesKey)
+        {
+            string aesKeyString = Convert.ToBase64String(aesKey);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "Insert Into AesKey(aesKey,serviceId)" +
+                " Values(cast(@aesKey as nvarchar(MAX))," +
+                " @serviceId);";
+            cmd.Parameters.AddWithValue("@aesKey", aesKeyString);
+            cmd.Parameters.AddWithValue("@serviceId", serviceId);
+            cmd.Connection = GetConnection();
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+            
+        }
+        public static void genKeyIv(Service service, int userId)
+        {
+            
+            using (Aes myAes = Aes.Create())
+            {
+                myAes.KeySize = 256;
+
+                byte[] usernameBytes = encryptInformation(service.username, myAes.Key);
+                byte[] passwdBytes = encryptInformation(service.password, myAes.Key);
+                byte[] notesBytes;
+
+                if (!testEmpty(service.notes))
+                {
+                    notesBytes = encryptInformation(service.notes, myAes.Key);
+                }
+                else
+                {
+                    notesBytes = null;
+                }
+
+                int serviceId = insertService(service.name, service.url, usernameBytes, passwdBytes, notesBytes, userId);
+
+                insertKey(serviceId, myAes.Key);
+            }
+
+            
+
+        }
+
+        private static byte[] encryptInformation(string input, byte[] aesKey)
+        {
+            byte[] encrypted;
+            byte[] IV;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.BlockSize = 128;
+                aesAlg.KeySize = 256;
+                aesAlg.Key = aesKey;
+                aesAlg.GenerateIV();
+                IV = aesAlg.IV;
+
+                aesAlg.Mode = CipherMode.CBC;
+
+                var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption. 
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(input);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+
+            }
+            var combinedIvCt = new byte[IV.Length + encrypted.Length];
+            Array.Copy(IV, 0, combinedIvCt, 0, IV.Length);
+            Array.Copy(encrypted, 0, combinedIvCt, IV.Length, encrypted.Length);
+
+            // Return the encrypted bytes from the memory stream. 
+            return combinedIvCt;
+        }
+
+        //Test Empty Fields
+        private static bool testEmpty(string testText)
+        {
+            return String.IsNullOrEmpty(testText) || String.IsNullOrWhiteSpace(testText);
+        }
+
+
+    }
+}
--- SecureNet/SecureNet/Pages/Manager/AddService.xaml.cs
+++ SecureNet/SecureNet/Pages/Manager/AddService.xaml.cs
@@ -12,8 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Data;
-using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.IO;
+using SecureNet.Classes;
 
 
 namespace SecureNet.Pages.Manager
@@ -23,10 +24,13 @@ namespace SecureNet.Pages.Manager
     /// </summary>
     public partial class AddService : Page
     {
+
+
         public AddService()
         {
             InitializeComponent();
             Style = (Style)FindResource(typeof(Page));
+
         }
 
         //Navigation : Back Button
@@ -35,21 +39,148 @@ namespace SecureNet.Pages.Manager
             this.NavigationService.Navigate(new Uri("/Pages/Manager/PassHome.xaml", UriKind.Relative));
         }
 
+        private int getUserId()
+        {
+            return 1; 
+        }
         private void ButtonSubmit_Click(object sender, RoutedEventArgs e)
         {
-           
-            using (SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["SecureNetCon"].ConnectionString))
+
+
+            string name = TextBoxName.Text;
+            string url = TextBoxUrl.Text;
+            string password = TextBoxPassword.Text;
+            string username = TextBoxUsername.Text;
+            string notes = TextBoxNotes.Text;
+
+
+            if (!testEmpty(name) && !testEmpty(url) && !testEmpty(password) && !testEmpty(username))
             {
 
-                SqlCommand cmd = new SqlCommand();
-                cmd.CommandText = "Insert Into Service(serviceName)" +
-                "Values(cast(@serviceName as nvarchar(50)));";
-                cmd.Parameters.AddWithValue("@serviceName", TextBoxName.Text);
-                cmd.Connection = connection;
-                connection.Open();
-                cmd.ExecuteNonQuery();
-                connection.Close();
+                //regex for password
+                //regex for username
+                //regex for notes
+                //Check for duplicates
+                //progressBar
+
+                if (testName(name) && testUrl(url))
+                {
+
+
+
+                    Service service = new Service();
+
+                    service.name = name;
+                    service.url = url;
+                    service.username = username;
+                    service.password = password;
+
+                    if (!testEmpty(notes))
+                    {
+                        service.notes = notes;
+                    }
+                    else
+                    {
+                        service.notes = null;
+                    }
+                    try
+                    {
+                    Service.genKeyIv(service, getUserId());
+                    errorMsg.Content = "Successfully added!";
+                    }
+                    catch(Exception ex)
+                    {
+                         errorMsg.Content = "Operation Error.Contact Tech Support.";
+                    }
+                }
+
+                else
+                {
+                    errorMsg.Content = "Error with Inputs. Please relook at the requirements";
+                }
+            }
+            else
+            {
+                errorMsg.Content = "Service Name, URL, Username and Password are compulsory fields.";
+
             }
         }
+
+        //Test Empty Fields
+        protected bool testEmpty(string testText)
+        {
+            return String.IsNullOrEmpty(testText) || String.IsNullOrWhiteSpace(testText);
+        }
+
+        //Text Max Chars
+        protected bool testMax(string text, int max)
+        {
+            return text.Length > max;
+
+        }
+
+        protected bool validUrl(string uriName)
+        {
+            Uri uriResult;
+            bool result = Uri.TryCreate(uriName, UriKind.Absolute, out uriResult)
+           && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            return result;
+        }
+
+        protected bool testName(string name)
+        {
+
+            Regex rgxName = new Regex("^[a-zA-Z0-9\x20]*$");
+
+            bool result = rgxName.IsMatch(name);
+            if (testMax(name, 50))
+            {
+
+                return false;
+
+            }
+            else if (!rgxName.IsMatch(name))
+            {
+
+
+                return false;
+            }
+            else
+            {
+
+                return true;
+            }
+        }
+
+        protected bool testUrl(string url)
+        {
+
+            bool test = validUrl(url);
+
+
+            if (testMax(url, 150))
+            {
+
+                return false;
+
+            }
+            else if (!validUrl(url))
+            {
+
+
+                return false;
+
+            }
+            else
+            {
+
+                return true;
+            }
+        }
+
+
+
+
+
     }
 }
