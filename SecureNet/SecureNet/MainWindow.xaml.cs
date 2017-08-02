203da0d4f0905dcce35e77987e31754e27503f2b
kcubey <keziakew98@gmail.com>
2017-08-03 00:40:46 +0800

    'sticky' cert complete, only requires to trust cert once. TODO: Ensure that cert is trusted

--- SecureNet/SecureNet/MainWindow.xaml.cs
+++ SecureNet/SecureNet/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using Fiddler;
 using System.Configuration;
 using System.Security.Cryptography.X509Certificates;
+using System.IO;
 
 namespace SecureNet
 {
@@ -24,6 +25,8 @@ namespace SecureNet
     /// </summary>
     public partial class MainWindow : Window
     {
+        protected string saveFile = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "PFX.PFX";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -37,97 +40,6 @@ namespace SecureNet
         private void OnClick(object sender, RoutedEventArgs e)
         {
             MainFrame.Source = new Uri(((Button)sender).CommandParameter.ToString(), UriKind.Relative);
-        }
-
-        void StartFiddler()
-        {
-            //FiddlerApplication.Startup(0, FiddlerCoreStartupFlags.Default);
-            //FiddlerApplication.Startup(8877, true, true);
-
-            if (FiddlerApplication.IsStarted())
-                FiddlerApplication.Shutdown();
-
-            DoCertificate();
-
-            FiddlerApplication.Startup(0, FiddlerCoreStartupFlags.Default);
-            Console.WriteLine("Fiddler Start");
-
-            FiddlerApplication.OnNotification += delegate (object sender, NotificationEventArgs oNEA)
-            {
-                Console.WriteLine("** NotifyUser: " + oNEA.NotifyString);
-            };
-
-            FiddlerApplication.Log.OnLogString += delegate (object sender, LogEventArgs oLEA)
-            {
-                Console.WriteLine("** LogString: " + oLEA.LogString);
-            };
-
-        }
-
-        private void RequestDetails(Session oSession)
-        {
-            Console.WriteLine("Request URL {0}", oSession.fullUrl);// getting only http traffic details
-
-        }
-
-
-        public void DoCertificate()
-        {
-            var checkCert = CertMaker.GetRootCertificate();
-
-            if (checkCert == null)
-            {
-                Console.WriteLine("Cert does not exist");
-                CertMaker.createRootCert();
-                CertMaker.trustRootCert();
-                Console.WriteLine("Cert created & trusted");
-            }
-
-            else if (checkCert != null)
-            {
-                Console.WriteLine("Cert exists");
-                bool checktrust = CertMaker.rootCertIsMachineTrusted();
-                if (checktrust == true)
-                {
-                    Console.WriteLine("is trusted");
-                }
-                else if (checktrust == false)
-                {
-                    Console.WriteLine("not trusted");
-                }
-            }
-
-            /*
-            if (!CertMaker.rootCertExists())
-            {
-                Console.WriteLine("root cert does not exist");
-
-                if (!CertMaker.createRootCert())
-                    return false;
-
-                if (!CertMaker.trustRootCert())
-                    return false;
-                /*
-                App.Configuration.UrlCapture.Cert =
-                    FiddlerApplication.Prefs.GetStringPref("fiddler.certmaker.bc.cert", null);
-                App.Configuration.UrlCapture.Key =
-                    FiddlerApplication.Prefs.GetStringPref("fiddler.certmaker.bc.key", null);
-                    */
-        }
-
-        //does not work
-        public void InstallCertificate()
-        {
-            X509Store certStore = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
-            // Try to open the store.
-
-            certStore.Open(OpenFlags.ReadOnly);
-            // Find the certificate that matches the name.
-            X509Certificate2Collection certCollection = certStore.Certificates.Find(X509FindType.FindBySubjectName, "DO_NOT_TRUST_FiddlerRoot", false);
-
-            X509Certificate2 certTry = new X509Certificate2(@"D:\PFX.PFX", "1", X509KeyStorageFlags.UserKeySet |
-                                        X509KeyStorageFlags.PersistKeySet |
-                                        X509KeyStorageFlags.Exportable);
         }
 
         private void EncryptConnString()
@@ -153,6 +65,126 @@ namespace SecureNet
             }
         }
 
+        void StartFiddler()
+        {
+            //FiddlerApplication.Startup(0, FiddlerCoreStartupFlags.Default);
+            //FiddlerApplication.Startup(8877, true, true);
+
+            if (FiddlerApplication.IsStarted())
+            {
+                FiddlerApplication.Shutdown();
+                Console.WriteLine("** Closing previous instance of Fiddler");
+            }
+
+            try
+            {
+                Console.WriteLine("** Retrieving Cert...");
+                RetrieveCertificate();
+            }
+            catch
+            {
+                Console.WriteLine("** Installing Cert...");
+                InstallCertificate();
+            }
+
+            FiddlerApplication.Startup(0, FiddlerCoreStartupFlags.Default);
+            Console.WriteLine("** Fiddler Start");
+
+            FiddlerApplication.OnNotification += delegate (object sender, NotificationEventArgs oNEA)
+            {
+                Console.WriteLine("** NotifyUser: " + oNEA.NotifyString);
+            };
+
+            FiddlerApplication.Log.OnLogString += delegate (object sender, LogEventArgs oLEA)
+            {
+                Console.WriteLine("** LogString: " + oLEA.LogString);
+            };
+
+        }
+
+        public void CheckCertificate()
+        {
+            var checkCert = CertMaker.GetRootCertificate();
+
+            if (checkCert == null)
+            {
+                Console.WriteLine("Cert does not exist");
+                CertMaker.createRootCert();
+                CertMaker.trustRootCert();
+                Console.WriteLine("Cert created & trusted");
+            }
+
+            else if (checkCert != null)
+            {
+                Console.WriteLine("Cert exists");
+                bool checktrust = CertMaker.rootCertIsMachineTrusted();
+                if (checktrust == true)
+                {
+                    Console.WriteLine("is trusted");
+                }
+                else if (checktrust == false)
+                {
+                    Console.WriteLine("not trusted");
+                }
+            }
+        }
+
+        private void RequestDetails(Session oSession)
+        {
+            Console.WriteLine("Request URL {0}", oSession.fullUrl);// getting only http traffic details
+
+        }
+        
+        public void InstallCertificate() //Create & trust cert, saves PFX file
+        {
+            CertMaker.createRootCert();
+            CertMaker.trustRootCert();
+
+            var certX = Fiddler.CertMaker.oCertProvider.GetCertificateForHost("<Machine Name>");
+            File.WriteAllBytes(saveFile, certX.Export(X509ContentType.SerializedCert));
+            //File.WriteAllBytes(@"C:\PFX.PFX", certX.Export(X509ContentType.SerializedCert));
+        }
+
+        public void RetrieveCertificate()
+        {
+            Console.WriteLine("** PFX file is at: " + saveFile);
+
+            X509Store certStore = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            // Try to open the store.
+
+            certStore.Open(OpenFlags.ReadOnly);
+            // Find the certificate that matches the name.
+            X509Certificate2Collection certCollection = certStore.Certificates.Find(X509FindType.FindBySubjectName, "DO_NOT_TRUST_FiddlerRoot", false);
+
+            X509Certificate2 certTry = new X509Certificate2(saveFile, "1", X509KeyStorageFlags.UserKeySet |
+                                        X509KeyStorageFlags.PersistKeySet |
+                                        X509KeyStorageFlags.Exportable);
+ /*
+            bool checktrust = certTry.Verify();
+
+            if (checktrust == true)
+            {
+                Console.WriteLine("** Cert verified");
+            }
+            else if (checktrust == false)
+            {
+                Console.WriteLine("** Cert UNverified");
+
+            }
+
+           
+            bool checktrust = CertMaker.rootCertIsMachineTrusted();
+            if (checktrust == true)
+            {
+                Console.WriteLine("** cert is trusted");
+            }
+            else if (checktrust == false)
+            {
+                Console.WriteLine("** cert not trusted");
+                InstallCertificate();
+            }*/
+        }
+
         private void FiddlerApplication_OnNotification(object sender, NotificationEventArgs e)
         {
             throw new NotImplementedException();
@@ -161,32 +193,17 @@ namespace SecureNet
         protected override void OnClosed(EventArgs e)
         {
             FiddlerApplication.oProxy.Detach();
-            CheckCertificate();
-            Console.WriteLine("Detach proxy");
+            Console.WriteLine("** Detached proxy");
 
             CertMaker.removeFiddlerGeneratedCerts(false);
-            //FiddlerApplication.Prefs.SetBoolPref("fiddler.certmaker.CleanupServerCertsOnExit", true);
-            CheckCertificate();
-            Console.WriteLine("Remove gen cert");
+            FiddlerApplication.Prefs.SetBoolPref("fiddler.certmaker.CleanupServerCertsOnExit", true);
+            Console.WriteLine("** Remove gen cert");
 
             FiddlerApplication.Shutdown();
-            CheckCertificate();
-            Console.WriteLine("Fiddler Closed");
+            Console.WriteLine("** Fiddler Closed");
         }
 
 
-        public void CheckCertificate()
-        {
-            var checkCert = CertMaker.GetRootCertificate();
-
-            if (checkCert == null) 
-            {
-                Console.WriteLine("Cert does not exist");
-            }
-            else if (checkCert != null)
-            {
-                Console.WriteLine("Cert exists");
-            }
-        }
+        
     }
 }
